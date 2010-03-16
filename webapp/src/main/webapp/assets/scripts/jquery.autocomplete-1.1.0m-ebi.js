aba0e1303a432fdfef13072e66f9a5b5df291e6f
kolais <kolais@2913f559-6b04-0410-9a09-c530ee9f5186>
2010-03-16 20:42:51 +0000

    fixed problem w/resetting cursor in Opera 9.64 - slightly cleaner approach
    git-svn-id: svn://bar.ebi.ac.uk/trunk/arrayexpress-interface@11363 2913f559-6b04-0410-9a09-c530ee9f5186

--- webapp/src/main/webapp/assets/scripts/jquery.autocomplete-1.1.0m-ebi.js
+++ webapp/src/main/webapp/assets/scripts/jquery.autocomplete-1.1.0m-ebi.js
@@ -230,7 +230,7 @@ $.Autocompleter = function(input, options) {
         var newTerm = replaceTermText(term, v);
         var pos = 0;
         var lastPos = 0;
-        var cursorPos = $input.selection().start;
+        var cursorPos = $input.caret().start;
         var shouldAppendSuffix = cursorPos == value.length;
 
         do {
@@ -328,7 +328,7 @@ $.Autocompleter = function(input, options) {
 
     function currentTerm(value, position)
     {
-        var cursorPosition = $(input).selection().start;
+        var cursorPosition = $(input).caret().start;
 
         var beforeCursor = cursorPosition > 0 ? value.substring(0, cursorPosition) : "";
         var afterCursor = value.substring(cursorPosition);
@@ -975,51 +975,6 @@ $.Autocompleter.Select = function (options, input, select, config) {
 		unbind: function() {
 			element && element.remove();
 		}
-	};
-};
-
-$.fn.selection = function(start, end) {
-	if (start !== undefined) {
-		return this.each(function() {
-			if( this.createTextRange ){
-				var selRange = this.createTextRange();
-				if (end === undefined || start == end) {
-					selRange.move("character", start);
-					selRange.select();
-				} else {
-					selRange.collapse(true);
-					selRange.moveStart("character", start);
-					selRange.moveEnd("character", end);
-					selRange.select();
-				}
-			} else if( this.setSelectionRange ){
-				this.setSelectionRange(start, end);
-			} else if( this.selectionStart ){
-				this.selectionStart = start;
-				this.selectionEnd = end;
-			}
-		});
 	}
-	var field = this[0];
-	if( field.selectionStart !== undefined ) {
-		return {
-			start: field.selectionStart,
-			end: field.selectionEnd
-		}
-    } else if ( field.createTextRange ) {
-		var range = document.selection.createRange(),
-			orig = field.value,
-			teststring = "<->",
-			textLength = range.text.length;
-		range.text = teststring;
-		var caretAt = field.value.indexOf(teststring);
-		field.value = orig;
-		this.selection(caretAt, caretAt + textLength);
-		return {
-			start: caretAt,
-			end: caretAt + textLength
-		}
-	}
-};
-
+}
 })(jQuery);
