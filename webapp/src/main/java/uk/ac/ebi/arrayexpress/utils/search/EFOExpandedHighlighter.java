e74e30cc1a9b6ef3b049d6293e34da10e9880e85
kolais <kolais@2913f559-6b04-0410-9a09-c530ee9f5186>
2010-09-16 15:51:03 +0000

    fix for #2423
    git-svn-id: svn://bar.ebi.ac.uk/trunk/arrayexpress-interface@14085 2913f559-6b04-0410-9a09-c530ee9f5186

--- webapp/src/main/java/uk/ac/ebi/arrayexpress/utils/search/EFOExpandedHighlighter.java
+++ webapp/src/main/java/uk/ac/ebi/arrayexpress/utils/search/EFOExpandedHighlighter.java
@@ -7,6 +7,7 @@ import org.apache.lucene.search.highlight.QueryScorer;
 import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.ac.ebi.arrayexpress.utils.RegexHelper;
 import uk.ac.ebi.arrayexpress.utils.saxon.search.IQueryHighlighter;
 import uk.ac.ebi.arrayexpress.utils.saxon.search.IndexEnvironment;
 import uk.ac.ebi.arrayexpress.utils.saxon.search.QueryInfo;
@@ -42,6 +43,10 @@ public class EFOExpandedHighlighter implements IQueryHighlighter
     private final String EFO_OPEN_MARK = "\u2035";
     private final String EFO_CLOSE_MARK = "\u2032";
 
+    private final RegexHelper SYN_AND_HIT_REGEX = new RegexHelper(HIT_OPEN_MARK + SYN_OPEN_MARK + "(.+)" + SYN_CLOSE_MARK + HIT_CLOSE_MARK, "g");
+    private final RegexHelper EFO_AND_SYN_REGEX = new RegexHelper(SYN_OPEN_MARK + EFO_OPEN_MARK + "(.+)" + EFO_CLOSE_MARK + SYN_CLOSE_MARK, "g");
+    private final RegexHelper EFO_AND_HIT_REGEX = new RegexHelper(HIT_OPEN_MARK + EFO_OPEN_MARK + "(.+)" + EFO_CLOSE_MARK + HIT_CLOSE_MARK, "g");
+
     public IQueryHighlighter setEnvironment( IndexEnvironment env )
     {
         this.env = env;
@@ -64,6 +69,11 @@ public class EFOExpandedHighlighter implements IQueryHighlighter
             String result = doHighlightQuery(queryInfo.getOriginalQuery(), fieldName, text, HIT_OPEN_MARK, HIT_CLOSE_MARK);
             result = doHighlightQuery(queryInfo.getSynonymPartQuery(), fieldName, result, SYN_OPEN_MARK, SYN_CLOSE_MARK);
             result = doHighlightQuery(queryInfo.getEfoExpansionPartQuery(), fieldName, result, EFO_OPEN_MARK, EFO_CLOSE_MARK);
+
+            result = EFO_AND_SYN_REGEX.replace(result, SYN_OPEN_MARK + "$1" + SYN_CLOSE_MARK);
+            result = SYN_AND_HIT_REGEX.replace(result, HIT_OPEN_MARK + "$1" + HIT_CLOSE_MARK);
+            result = EFO_AND_HIT_REGEX.replace(result, HIT_OPEN_MARK + "$1" + HIT_CLOSE_MARK);
+
             return result;
         }
     }
