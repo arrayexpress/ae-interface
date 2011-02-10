f6fe666543e064c628b6733131b6b7b1baed7736
kolais <kolais@2913f559-6b04-0410-9a09-c530ee9f5186>
2011-02-10 11:36:43 +0000

    added placeholder status checker responder (for external monitoring/load balancing)
    git-svn-id: svn://bar.ebi.ac.uk/trunk/arrayexpress-interface@16642 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ webapp/src/main/java/uk/ac/ebi/arrayexpress/servlets/StatusServlet.java
@@ -0,0 +1,58 @@
+package uk.ac.ebi.arrayexpress.servlets;
+
+/*
+ * Copyright 2009-2011 European Molecular Biology Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import uk.ac.ebi.arrayexpress.app.ApplicationServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class StatusServlet extends ApplicationServlet
+{
+    private static final long serialVersionUID = 8929729058610937695L;
+
+    protected boolean canAcceptRequest( HttpServletRequest request, RequestType requestType )
+    {
+        return (requestType == RequestType.GET || requestType == RequestType.POST);
+    }
+
+    // Respond to HTTP requests from browsers.
+    protected void doRequest( HttpServletRequest request, HttpServletResponse response, RequestType requestType )
+            throws ServletException, IOException
+    {
+        response.setContentType("text/plain; charset=ISO-8859-1");
+        response.addHeader("Pragma", "no-cache");
+        response.addHeader("Cache-Control", "no-cache");
+        response.addHeader("Cache-Control", "must-revalidate");
+        response.addHeader("Expires", "Fri, 16 May 2008 10:00:00 GMT"); // some date in the past
+
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            out.println("OK");
+        } finally {
+            if (null != out) {
+                out.flush();
+                out.close();
+            }
+        }
+    }
+}
