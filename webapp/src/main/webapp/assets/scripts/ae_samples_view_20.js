cf1ac4fa628afd366ee832ed0bd0ce8b57a151dc
kolais <kolais@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-06-28 23:38:01 +0000

    - improved samples view
    git-svn-id: svn://bar.ebi.ac.uk/trunk/arrayexpress-interface@20190 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ webapp/src/main/webapp/assets/scripts/ae_samples_view_20.js
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2009-2012 European Molecular Biology Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+(function($, undefined) {
+    if($ == undefined)
+        throw "jQuery not loaded";
+
+    $.fn.extend({
+
+        aeSampleTableSorter: function() {
+            return this.each(function() {
+                new $.AESampleTableSorter(this);
+            });
+        }
+    });
+
+    $.AESampleTableSorter = function( elt ) {
+
+    };
+
+    function
+    tableAdjustColWidth( eltName )
+    {
+        var tdElt = $(eltName);
+        tdElt.width(tdElt.children().first().width())
+    }
+
+    function
+    tableAdjustFillerHeight( srcEltName, dstEltName )
+    {
+        var srcElt = $(srcEltName);
+        $(dstEltName).height(srcElt.height() - srcElt.children().first().height());
+    }
+
+    function
+    updateScrollShadow()
+    {
+        if (scrollElt.scrollLeft() !== 0) {
+            if (!shadowElt.hasClass("left-shadow")) {
+                shadowElt.addClass("left-shadow");
+            }
+        } else {
+            shadowElt.removeClass("left-shadow");
+        }
+        if (scrollableElt.width() - scrollElt.scrollLeft() !== scrollElt.width()) {
+            if (!scrollElt.hasClass("right-shadow")) {
+                scrollElt.addClass("right-shadow");
+            }
+        } else {
+            scrollElt.removeClass("right-shadow");
+        }
+    }
+
+    $(function() {
+
+        if ($.query == undefined)
+            throw "jQuery.query not loaded";
+
+        var sortby = $.query.get("sortby") || "col_1";
+        var sortorder = $.query.get("sortorder") || "ascending";
+
+        var localPath = /(\/.+)$/.exec(decodeURI(window.location.pathname))[1];
+
+        tableAdjustColWidth("td.left_fixed");
+        tableAdjustColWidth("td.right_fixed");
+        tableAdjustFillerHeight("div.attr_table_scroll", "td.bottom_filler");
+
+        if (false && !$.browser.msie) {
+            shadowElt = $("td.middle_scrollable");
+            scrollElt = shadowElt.children().first();
+            scrollableElt = scrollElt.children().first();
+
+            scrollElt.scroll( function() { updateScrollShadow() });
+            $(window).resize( function() { updateScrollShadow() });
+            updateScrollShadow();
+        }
+
+        $("#ae_results_box").find("th.sortable").each( function() {
+            var thisObj = $(this);
+            var colname = /(col_\d+)/.exec(thisObj.attr("class"))[1];
+
+            // so the idea is to set default sorting for all columns except the "current" one
+            // (which will be inverted) against its current state
+            var newOrder = (colname === sortby) ? ("ascending" === sortorder ? "descending" : "ascending"): "ascending";
+            var queryString = $.query.set("sortby", colname).set("sortorder", newOrder).toString();
+
+            thisObj.wrapInner("<a href=\"" + localPath + queryString + "\" title=\"Click to sort table by this column\"/>");
+        });
+    });
+
+})(window.jQuery);
